e6fce0eddbe68bc145461b8fe31d0cb5c3c544e7
josiqueli <105284716+josiqueli@users.noreply.github.com>
2024-03-08 09:12:30 -0300

    ok

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
-const botoes = document.querySelectorAll(".botao")
+const botoes = document.querySelectorAll(".botao");
 
 for(let i=0;i <botoes.length;i++){
     botoes[i].onlink = function(){
 
-            for(Let j=0;j<botoes.length;j++){
+            for(let j=0;j<botoes.length;j++){
                 botoes[j].classList.remove("ativo");
             }
+
             botoes[i].classList.add("ativo");
     }
 }
