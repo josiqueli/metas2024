99e24a52c444089a1c39eacce7f6326feb7dbda1
josiqueli <105284716+josiqueli@users.noreply.github.com>
2024-04-25 21:10:45 -0300

    ok

--- main.js
+++ main.js
@@ -5,14 +5,13 @@ for (let i = 0; i < botoes.length; i++) {
     botoes[i].onclick = function () {
 
         for (let j = 0; j < botoes.length; j++) {
-            if(botoes[j]) {
-                botoes[j].classList.remove("ativo");
-            }
-            if(textos[j]) {
-                textos[j].classList.remove("ativo");
-            }
+            botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo");
         }
-        
+
+        botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo");
+    }
 }
 //cria as constantes e a lista "tempo" dos objetivos ou metas
 const contadores = document.querySelectorAll(".contador");
